da9c76b3f936aba509b11cfad3e5305dff1e6222
DEONTAY JEFFERSON <djefferson46@gmail.com>
2021-07-23 08:30:04 -0700

    Corrected the static issue on methods

--- MethodOverLoad/Program.cs
+++ MethodOverLoad/Program.cs
@@ -3,32 +3,37 @@
 namespace MethodOverLoad
 {
     class Program
-    {
-            static int Add(int num1, int num2)
+    {       
+        //defining a simple adding method
+           int Add(int num1, int num2)
             {
                 return num1 + num2;
             }
 
-            static decimal Add(decimal num1, decimal num2)
+        //overloading the method to accept decimals
+             decimal Add(decimal num1, decimal num2)
             {
                 return num1 + num2;
             }
 
-        static string Add(int num1, int num2, bool check)
-        {
-            var str = "";
+        //overloading the method to accepts bools and return a string
+           static string Add(bool check,int first, int second )
+                {
+                    var str = "";
 
-            if (check && num1 + num2 == 1) {  str = $"{num1 + num2} dollar ";}
-            else { str = $"{num1 + num2} dollars "; }
+                    if (check && first + second == 1) {  str = $"{first + second} dollar ";}
+                    else { str = $"{first + second} dollars "; }
 
-            return str;
-        }
+                    return str;
+                }
 
 
         static void Main(string[] args)
         {
-
-
+            //calling test
+            Add(2, 3);
+            Add(2.3m, 4.5m);
+            Add(true, 30, 40);
            
             Console.WriteLine("Hello World!");
         }
